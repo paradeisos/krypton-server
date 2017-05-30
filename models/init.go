f2c6c4c8b75bcbc61643f2d570014873258a2304
foreversmart <njutree@gmail.com>
2017-05-30 21:35:41 +0800

    refactor models init

--- models/init.go
+++ models/init.go
@@ -1,13 +1,34 @@
 package models
 
+import "github.com/astaxie/beego"
+
 var (
 	mongo *Model
 )
 
-func SetupModel(model *Model) {
-	mongo = model
+func InitMongo() {
+	mongoHost := beego.AppConfig.String("mongo_host")
+	mongoUser := beego.AppConfig.String("mongo_user")
+	mongoPassword := beego.AppConfig.String("mongo_password")
+	mongoDatabase := beego.AppConfig.String("mongo_database")
+	mongoMode := beego.AppConfig.String("mongo_mode")
+	mongoPool := beego.AppConfig.DefaultInt("mongo_pool", 0)
+	mongoTimeout := beego.AppConfig.DefaultInt("mongo_timeout", 0)
+
+	option := &Option{
+		Host:     mongoHost,
+		User:     mongoUser,
+		Password: mongoPassword,
+		Database: mongoDatabase,
+		Mode:     mongoMode,
+		Pool:     mongoPool,
+		Timeout:  mongoTimeout,
+	}
+
+	mongo = NewModel(option)
+
 }
 
-func Model() *Model {
+func Mongo() *Model {
 	return mongo
 }
