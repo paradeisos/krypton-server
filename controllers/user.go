b3f2ea235cb8daffd3c57b41372df14d44b93ae0
foreversmart <njutree@gmail.com>
2017-05-31 00:05:06 +0800

    add base use register

--- controllers/user.go
+++ controllers/user.go
@@ -1,12 +1,36 @@
 package controllers
 
 import (
+	"encoding/json"
 	"github.com/astaxie/beego"
+	"krypton-server/models"
+	"net/http"
 )
 
-type LoginController struct {
+type User struct {
 	beego.Controller
 }
 
-func (c *LoginController) Login() {
+func (c *User) Login() {
+}
+
+// register
+func (c *User) Post() {
+	var params *UserRegisterParams
+	resp := &Response{}
+	err := json.Unmarshal(c.Ctx.Input.RequestBody, &params)
+	if err != nil {
+		beego.Error(err)
+
+	}
+
+	user := models.User.NewUserModel(params.Username, params.Email, params.Password, "")
+	err = user.Save()
+	if err != nil {
+		beego.Error(err)
+	}
+
+	resp.Status = http.StatusOK
+	c.Data["json"] = resp
+	c.ServeJSON()
 }
