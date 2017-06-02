29c14ca470a4ac00d7e53db9d1f97944a311cc1b
csxuejin <602296435@qq.com>
2017-06-02 11:48:32 +0800

    tomato model

---
+++ models/tomato.go
@@ -0,0 +1,94 @@
+package models
+
+import (
+	"time"
+
+	mgo "gopkg.in/mgo.v2"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+var (
+	Tomato *_Tomato
+
+	tomatoCollection = "tomato"
+	tomatoIndexes    = []mgo.Index{}
+)
+
+type _Tomato struct{}
+
+type TomatoModel struct {
+	Id        bson.ObjectId `bson:"_id" json:"-"`
+	Uid       string        `bson:"uid" json:"uid"`
+	Start     time.Time     `bson:"start" json:"start"`
+	End       time.Time     `bson:"end" json:"end"`
+	Desc      string        `bson:"desc" json:"desc"`
+	Finished  bool          `bson:"finished" json:"finished"`
+	CreatedAt time.Time     `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time     `bson:"updated_at" json:"updated_at"`
+
+	isNewRecord bool `bson:"-"`
+}
+
+func (_ *_Tomato) NewTomatoModel(uid string, start, end time.Time, desc string, finished bool) *TomatoModel {
+	return &TomatoModel{
+		Id:          bson.NewObjectId(),
+		Uid:         uid,
+		Start:       start,
+		End:         end,
+		Desc:        desc,
+		Finished:    finished,
+		isNewRecord: true,
+	}
+}
+
+func (tomato *TomatoModel) Save() (err error) {
+	if !tomato.Id.Valid() {
+		return ErrInvalidId
+	}
+
+	Tomato.Query(func(c *mgo.Collection) {
+		t := time.Now()
+
+		if tomato.isNewRecord {
+			tomato.CreatedAt = t
+			tomato.UpdatedAt = t
+
+			if err = c.Insert(tomato); err == nil {
+				tomato.isNewRecord = false
+			}
+		} else {
+			migration := bson.M{
+				"$set": bson.M{
+					"desc":       tomato.Desc,
+					"start":      tomato.Start,
+					"end":        tomato.End,
+					"updated_at": t,
+				},
+			}
+
+			err = c.UpdateId(tomato.Id, migration)
+		}
+	})
+
+	return
+}
+
+func (_ *_Tomato) FindByUid(uid string) (tomatoes []*TomatoModel, err error) {
+	if uid == "" || uid != "" && !bson.IsObjectIdHex(uid) {
+		err = ErrInvalidId
+		return
+	}
+
+	Tomato.Query(func(c *mgo.Collection) {
+		err = c.Find(bson.M{
+			"uid": uid,
+		}).All(&tomatoes)
+	})
+
+	return
+}
+
+func (_ *_Tomato) Query(query func(c *mgo.Collection)) {
+	Mongo().Query(tomatoCollection, tomatoIndexes, query)
+}
---
+++ models/tomato_test.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golib/assert"
+	uuid "github.com/satori/go.uuid"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func Test_NewTomatoModel(t *testing.T) {
+	assertion := assert.New(t)
+	start := time.Now()
+	end := start.Add(25 * time.Minute)
+	uid := uuid.NewV4().String()
+
+	tomato := Tomato.NewTomatoModel(uid, start, end, "test", true)
+	assertion.Equal(start, tomato.Start)
+	assertion.Equal(end, tomato.End)
+	assertion.Equal(true, tomato.Finished)
+}
+
+func Test_FindByUid(t *testing.T) {
+	assertion := assert.New(t)
+	start := time.Now()
+	end := start.Add(25 * time.Minute)
+	uid := bson.NewObjectId().Hex()
+
+	tomato := Tomato.NewTomatoModel(uid, start, end, "test", true)
+	assertion.Nil(tomato.Save())
+
+	// duplicated save
+	assertion.Nil(tomato.Save())
+
+	tomatoes, err := Tomato.FindByUid(uid)
+	assertion.Nil(err)
+	assertion.Equal(1, len(tomatoes))
+	assertion.Equal(tomato.Id, tomatoes[0].Id)
+}
